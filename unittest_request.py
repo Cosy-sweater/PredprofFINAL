9acdf4e4efdcc291721a50d5a1147dc8a24279cb
LooKing <looking070@gmail.com>
2024-03-31 13:17:28 +0300

    add unittest

--- unittest_request.py
+++ unittest_request.py
@@ -1,27 +1,46 @@
 import unittest
-from request_api import vvod
+import requests
+from request_api import room_info, days_info
 
 
 class TestReverse(unittest.TestCase):
-    def test_empty(self):
-        self.assertEqual(reverse(''), '')
+    def test_empty_d_i(self):
+        self.assertEqual(type(days_info()), list)
+        datas = days_info()
+        print("Это unittest -", datas)
 
-    def test_one_symbol(self):
-        self.assertEqual(len(reverse('a')), 1)
+    def test_empty_r_i(self):
+        with self.assertRaises(RuntimeError):
+            room_info('', '', '')
 
-    def test_palindrom(self):
-        self.assertEqual(reverse("aboba"), "aboba")
+    def test_room_exist_data(self):
+        self.assertEqual(type(room_info("25", "01", "23")), dict)
 
-    def test_simple(self):
-        self.assertEqual(reverse("pohru"), "urhop")
+        datas = days_info()
+        for day in range(0, 21, 6):
+            for month in range(0, 13, 6):
+                for year in range(60, -1, -12):
+                    data = '-'.join([f"{day}".rjust(2, '0'), f"{month}".rjust(2, '0'), f"{year}".rjust(2, '0')])
+                    if data not in datas:
+                        with self.assertRaises(RuntimeError):
+                            room_info(data[:2], data[3:5], data[6:8])
+                        print(data)
 
-    def test_noniterable(self):
-        with self.assertRaises(TypeError):
-            reverse(22)
+    def test_room_no_exist_data(self):
+        with self.assertRaises(RuntimeError):
+            room_info("95", "011", "23")
 
-    def test_iterable(self):
-        with self.assertRaises(TypeError):
-            reverse([42, "asas", "bnm<>"])
+    def test_wrong_format(self):
+        with self.assertRaises(RuntimeError):
+            room_info([42, 1, 24])
+        with self.assertRaises(RuntimeError):
+            room_info('0014')
+        with self.assertRaises(RuntimeError):
+            room_info(10, 3, 45)
+        with self.assertRaises(RuntimeError):
+            room_info(*[42, 1, 24])
+        with self.assertRaises(RuntimeError):
+            room_info({'d': 11, 'm': 11, 'y': 11})
 
 
 if __name__ == '__main__':
